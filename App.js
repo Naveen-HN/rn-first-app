2729e9803b0d5bbb9c794be661487857bdacacbd
Naveen-HN <hnnaveen93@gmail.com>
2020-04-20 11:48:50 -0400

    added textinput and button with basic styling

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [output, current] = useState("Hello World!");
   return (
     <View style={styles.container}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TextInput
+          placeholder="Enter text here"
+          style={{ width: "70%", borderColor: "black", borderWidth: 1 }}
+        />
+        <Button title="Add" />
+      </View>
       <Text>{output}</Text>
       <Button
         title="Click me to change"
